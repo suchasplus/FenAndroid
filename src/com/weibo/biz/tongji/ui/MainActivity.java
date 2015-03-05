a38e0dc2f854660a1e1e815135d7131bd844e97c
suchasplus <suchasplus@gmail.com>
2015-03-05 21:06:36 +0800

    refactor MainActivity to StatActivity, add new MainActivity

--- src/com/weibo/biz/tongji/ui/MainActivity.java
+++ src/com/weibo/biz/tongji/ui/MainActivity.java
@@ -1,46 +1,15 @@
 package com.weibo.biz.tongji.ui;
 
-import android.app.Fragment;
-import android.content.Intent;
-import android.net.Uri;
-import android.view.Menu;
-import android.view.MenuItem;
-import com.weibo.biz.tongji.R;
-import com.weibo.biz.tongji.base.SingleFragmentActivity;
+import android.os.Bundle;
+import com.weibo.biz.tongji.base.BaseActivity;
 
-public class MainActivity extends SingleFragmentActivity {
+/**
+ * Powered by suchasplus @15/3/5 21:03
+ */
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected Fragment createFragment() {
-        return new FenListFragment();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent i;
-        switch (item.getItemId()) {
-
-            case R.id.report:
-                i = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                        "mailto", "wenyue1@staff.weibo.com", null));
-                i.putExtra(Intent.EXTRA_SUBJECT, "粉统计Android Issue");
-                i.putExtra(Intent.EXTRA_TEXT, "Your error report here...");
-                startActivity(Intent.createChooser(i, "Report Problem"));
-                break;
-
-            case R.id.website:
-                i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("http://tongji.biz.weibo.com"));
-                startActivity(i);
-                break;
-        }
-        return true;
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
     }
 }
